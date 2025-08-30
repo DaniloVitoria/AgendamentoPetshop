d1c6080dd48526253e4dbe7e4bc07629ed0b5d0b
Danilo <danilovitoriastf@gmail.com>
2025-08-30 00:45:37 -0300

    fist commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// All imports
+
+"use strict"
+
+// CSS
+import "./styles/global.css"
+import "./styles/utils.css"
+import "./styles/container__app.css"
+import "./styles/containerall.css"
+import "./styles/schedule.css"
+import "./styles/list.css"
+import "./styles/newschedule.css"
